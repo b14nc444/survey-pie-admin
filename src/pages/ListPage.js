452e7db8bc92729e91f05eb2d4b07d01ec27cd46
b14nc444 <b14nc4@naver.com>
2025-04-05 18:30:20 +0900

    feat: useSWR을 이용하여 API 연동

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,14 @@
+import useSWR from 'swr';
+
 import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
 
 function ListPage() {
+  const { data, error } = useSWR('/surveys', fetcher);
+
+  if (error) return <div> 에러 발생 </div>;
+  if (!data) return <div> 로딩중 </div>;
+
   return <MainLayout> ListPage </MainLayout>;
 }
 
