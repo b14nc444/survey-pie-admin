1d0b752d7ba0f399104ac8eb632f10758b2e0113
b14nc444 <b14nc4@naver.com>
2025-04-06 01:25:11 +0900

    feat: 설문 데이터 관리 기능 개선 - 미들웨어 핸들링

--- src/pages/BuilderPage.js
+++ src/pages/BuilderPage.js
@@ -1,9 +1,12 @@
 import { Col, Input, Row } from 'antd';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import OptionSection from '../components/OptionSection';
 import PreviewSection from '../components/PreviewSection';
 import MainLayout from '../layouts/MainLayout';
+import fetchSurvey from '../service/fetchSurvey';
 import {
   addQuestion,
   deleteQuestion,
@@ -13,8 +16,23 @@ import {
 } from '../stores/survey/surveySlice';
 
 function BuilderPage() {
-  const survey = useSelector((state) => state.survey);
+  const survey = useSelector((state) => state.survey.data);
+  const loading = useSelector((state) => state.survey.loading);
+  const error = useSelector((state) => state.survey.error);
   const dispatch = useDispatch();
+  const params = useParams();
+
+  useEffect(() => {
+    dispatch(fetchSurvey(params.surveyId));
+  }, [dispatch, params.surveyId]);
+
+  if (!survey || loading) {
+    return <div> Loading... </div>;
+  }
+
+  if (error) {
+    return <div> Error: {error} </div>;
+  }
 
   return (
     <MainLayout>
