4c27626655c1c529ba55221d9108070dd2d606e7
b14nc444 <b14nc4@naver.com>
2025-04-05 23:10:18 +0900

    feat: 질문 카드 관리 기능 구현 - 순서 변경, 삭제 기능 추가

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,8 @@
+import { DeleteOutlined, DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import styled from 'styled-components';
 
-function Card({ title, desc, children }) {
+function Card({ title, desc, children, onUpButtonClick, onDownButtonClick, onDeleteButtonClick }) {
   return (
     <CardWrapper>
       {' '}
@@ -8,15 +10,43 @@ function Card({ title, desc, children }) {
         <Title> {title} </Title> <Desc> {desc} </Desc>{' '}
       </Heading>{' '}
       <Body> {children} </Body>{' '}
+      <ButtonGroupWrapper>
+        <ButtonGroup>
+          <Button type="text" icon={<UpOutlined />} onClick={onUpButtonClick} />{' '}
+          <Button type="text" icon={<DeleteOutlined />} onClick={onDeleteButtonClick} />{' '}
+          <Button type="text" icon={<DownOutlined />} onClick={onDownButtonClick} />{' '}
+        </ButtonGroup>{' '}
+      </ButtonGroupWrapper>{' '}
     </CardWrapper>
   );
 }
+
+const ButtonGroupWrapper = styled.div`
+  position: absolute;
+  left: calc(100%);
+  top: 0;
+  display: none;
+`;
+
+const ButtonGroup = styled.div`
+  background: #ffffff;
+  margin-left: 16px;
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+`;
 
 const CardWrapper = styled.div`
   border: 1px solid #dddddd;
   width: 400px;
   margin: 30px auto;
   background-color: #ffffff;
+  position: relative;
+
+  &:hover {
+    ${ButtonGroupWrapper} {
+      display: block;
+    }
+  }
 `;
 
 const Heading = styled.div`
@@ -37,5 +67,4 @@ const Desc = styled.div`
 const Body = styled.div`
   padding: 16px;
 `;
-
 export default Card;
